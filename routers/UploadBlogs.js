cce1221bce34a3c59ebd6efb7f3aa2e9dd36dfc5
Rahul <chrahulofficial@gmail.com>
2024-03-29 14:34:08 +0530

    dn

--- routers/UploadBlogs.js
+++ routers/UploadBlogs.js
@@ -13,10 +13,13 @@ blogUpload.post("/blogs/:userjwt", async (req, res) => {
     }
     // Protect the server from spamming reqs
     const reqData = userReqCounts.has(token);
+
     if (reqData) {
       const currentTime = new Date().getTime();
+
       if (currentTime - reqData.startTime >= Timelimit) {
         reqData.startTime = currentTime;
+
         reqData.count = 1;
       } else if (reqData.counter > maxreq) {
         res.status(429).json({ error: "Too many requests" });
@@ -29,9 +32,12 @@ blogUpload.post("/blogs/:userjwt", async (req, res) => {
         image,
         sections,
       });
+
       await blog.save();
+
       userReqCounts.set(token, { startTime: new Date().getTime(), count: 1 });
       console.log(userReqCounts);
+
       res.json({ messgae: "sucess" }).status(200);
     }
   } catch (error) {
@@ -40,15 +46,22 @@ blogUpload.post("/blogs/:userjwt", async (req, res) => {
   }
 });
 
-blogUpload.put("/blogs/:userjwt", async (req, res) => {
+blogUpload.put("/blogs/:userjwt/:blogid", async (req, res) => {
   try {
     const token = req.params.userjwt;
-    const userexits = await UserModel.findById(token);
-    if (!userexits) {
+    const blogId = req.params.blogid;
+
+    const userExits = await UserModel.findById(token);
+    const blog = await Blogs.findById(blogId);
+
+    if (!userExits) {
       res.status(404).json({ error: "User not found" });
-    } else {
-      console.log(userexits);
     }
+
+    if (!blog) {
+      res.status(404).json({ error: "Blog not found" });
+    }
+    res.send(blog);
   } catch (error) {
     console.log(error);
   }
