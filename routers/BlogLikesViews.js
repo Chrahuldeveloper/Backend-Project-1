1955750bdcdbd26d743fcb25d77f3b84e38ed422
Rahul <chrahulofficial@gmail.com>
2024-03-30 21:53:11 +0530

    done

--- routers/BlogLikesViews.js
+++ routers/BlogLikesViews.js
@@ -27,8 +27,26 @@ blogViews.put("/blog/views/:userid/:blogid", async (req, res) => {
   }
 });
 
-blogLikes.get("/blog/likes/:userid/:blogid", async (req, res) => {
+blogLikes.put("/blog/likes/:userid/:blogid", async (req, res) => {
   try {
+    const blogId = req.params.blogid;
+    const userId = req.params.userid;
+
+    const blog = await Blogs.findById(blogId);
+
+    if (!userId) {
+      res.status(404).json({ message: "User Not found" });
+    }
+
+    if (!blog) {
+      res.status(404).json({ message: "Blog not found:(" });
+    }
+
+    blog.likes += 1;
+
+    const updatedBlog = await blog.save();
+
+    console.log(updatedBlog);
   } catch (error) {
     console.log(error);
   }
